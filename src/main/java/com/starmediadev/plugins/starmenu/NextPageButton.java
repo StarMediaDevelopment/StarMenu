b51da055cef265e1d7ac0024a19a6928ecdac2da
Firestar311 <firestar311mc@gmail.com>
2021-07-05 10:23:39 -0500

    Added click actions for the next and previous page buttons

--- src/main/java/com/starmediadev/plugins/starmenu/NextPageButton.java
+++ src/main/java/com/starmediadev/plugins/starmenu/NextPageButton.java
@@ -1,12 +1,26 @@
 package com.starmediadev.plugins.starmenu;
 
 import com.starmediadev.plugins.starmcutils.builder.ItemBuilder;
+import com.starmediadev.plugins.starmcutils.util.MCUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 
 public class NextPageButton extends Button {
 
     public NextPageButton(Material material, String color) {
         this(material, color, -1);
+        setLeftClickAction((player, menu, click) -> {
+            int totalPages = menu.getTotalPages();
+            int currentPage = menu.getCurrentPage();
+            
+            if (currentPage == totalPages) {
+                player.sendMessage(MCUtils.color("&cYou are already at the last page."));
+                return;
+            }
+            
+            menu.setCurrentPage(++currentPage);
+            Bukkit.getScheduler().runTaskLater(StarMenu.getPlugin(StarMenu.class), () -> player.openInventory(menu.getInventory()), 1L);
+        });
     }
 
     public NextPageButton(Material material, String color, int staticIndex) {
--- src/main/java/com/starmediadev/plugins/starmenu/PreviousPageButton.java
+++ src/main/java/com/starmediadev/plugins/starmenu/PreviousPageButton.java
@@ -1,12 +1,26 @@
 package com.starmediadev.plugins.starmenu;
 
 import com.starmediadev.plugins.starmcutils.builder.ItemBuilder;
+import com.starmediadev.plugins.starmcutils.util.MCUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 
 public class PreviousPageButton extends Button {
 
     public PreviousPageButton(Material material, String color) {
         this(material, color, -1);
+        setLeftClickAction((player, menu, click) -> {
+            int totalPages = menu.getTotalPages();
+            int currentPage = menu.getCurrentPage();
+
+            if (currentPage == 1) {
+                player.sendMessage(MCUtils.color("&cYou are already at the first page."));
+                return;
+            }
+
+            menu.setCurrentPage(--currentPage);
+            Bukkit.getScheduler().runTaskLater(StarMenu.getPlugin(StarMenu.class), () -> player.openInventory(menu.getInventory()), 1L);
+        });
     }
 
     public PreviousPageButton(Material material, String color, int staticIndex) {
